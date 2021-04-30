2cb1097a1fa4ff86feb9cb940acfceb3d1c6b909
Md Rizwan Akram <akramrizwan8@gmail.com>
2021-04-30 17:34:57 +0530

    Adds custom hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havenese" />
-      <Pet name="Pepper" animal="Bird" breed="Wild" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
--- src/Pet.js
+++ src/Pet.js
@@ -1,22 +1,21 @@
 import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-  
   return (
     <div>
       <h1>{name}</h1>
       <h2>{animal}</h2>
       <h2>{breed}</h2>
     </div>
-  )
+  );
 }
 
 /**
- * 
+ *
  * for reference purpose
  */
 // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, name),
-  //   React.createElement("h2", {}, animal),
-  //   React.createElement("h2", {}, breed),
-  // ]);
+//   React.createElement("h1", {}, name),
+//   React.createElement("h2", {}, animal),
+//   React.createElement("h2", {}, breed),
+// ]);
